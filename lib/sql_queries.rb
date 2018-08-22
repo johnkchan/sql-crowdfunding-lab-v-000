f4b2ce223fc68294f5d16e68c9e158913709a6fe
Jonathan Chan <johnkchan252@gmail.com>
2018-08-22 02:08:56 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT p.title, SUM(pl.amount) - p.funding_goal as diff
   FROM projects as p
   INNER JOIN pledges as pl ON p.id = pl.project_id
-  WHERE diff >= 0
   GROUP BY pl.project_id"
 end
 
